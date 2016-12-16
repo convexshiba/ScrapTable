50020b886fc1ca0b0fdf17f090d0539f497051a4
coderant <zj45499@gmail.com>
2016-12-16 00:48:04 -0500

    change computer & refactor

--- src/doh/3_address_dict_example.py
+++ src/doh/3_address_dict_example.py
@@ -1,4 +1,4 @@
-from generate_location_to_db_3 import load_camis_address
+from doh.generate_location_to_db_3 import load_camis_address
 
 address_dict = load_camis_address()
 print(address_dict["41468766"])
